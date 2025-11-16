f1380251dc8ae7270572070c0501bd92f128b1b8
Muhammad Arya Dzaky Arenanto <aryadzaky8494@gmail.com>
2025-11-16 12:46:56 +0700

    feat(add) : test

--- port_v2/lib/getHome.ts
+++ port_v2/lib/getHome.ts
@@ -2,7 +2,7 @@ import { Hero } from "@/types/homeType";
 import { api } from "./api";
 
 export const getHero = async (lang: string): Promise<Hero> => {
-  const { data } = await api.get(`    /api/${lang}/hero`);
+  const { data } = await api.get(`/api/${lang}/hero`);
 
   return data;
 };
